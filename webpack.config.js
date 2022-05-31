544c4456957698326b1df1a3e1d7ff3e25fc6f6e
Christian Bermeo <christiancabp@gmail.com>
2022-05-31 18:03:04 -0400

    final piece

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
+const { resolve } = require("path");
+
 module.exports = {
-  entry: [
-    './client/index.js'
-  ],
+  entry: ["./client/index.js"],
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: "./public/bundle.js",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: [
-            '@babel/preset-react'
-          ]
-        }
-      }
-    ]
-  }
-}
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+};
